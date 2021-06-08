0f7a4c7b91a3f43e66bd8a825df31e071cf5a8a2
mcavolowsky <mark.cavolowsky@gmail.com>
2021-06-08 19:24:05 -0400

    Weight sampling implementation.

--- torch_ac/__init__.py
+++ torch_ac/__init__.py
@@ -1,3 +1,3 @@
-from torch_ac.algos import A2CAlgo, PPOAlgo, MultiQAlgo
+from torch_ac.algos import A2CAlgo, PPOAlgo, MultiQAlgo, WeightedPPOAlgo
 from torch_ac.model import ACModel, RecurrentACModel
 from torch_ac.utils import DictList
--- torch_ac/algos/__init__.py
+++ torch_ac/algos/__init__.py
@@ -1,3 +1,4 @@
 from torch_ac.algos.a2c import A2CAlgo
 from torch_ac.algos.ppo import PPOAlgo
 from torch_ac.algos.multiQ import MultiQAlgo
+from torch_ac.algos.weightedPPO import WeightedPPOAlgo
